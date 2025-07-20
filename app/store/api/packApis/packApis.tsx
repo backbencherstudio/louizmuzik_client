2d160dc59783fd8a2e0e0ae40ff149da66e95619
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-07-20 14:15:44 +0600

    Update  pack Api added

--- app/store/api/packApis/packApis.tsx
+++ app/store/api/packApis/packApis.tsx
@@ -1,53 +1,60 @@
 import { baseApi } from "../baseApi";
 
-
 export const packApis = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        createPack: builder.mutation({
-            query: (formData: FormData) => ({
-                url: '/pack/create-pack',
-                method: 'POST',
-                body: formData,
-            }),
-            invalidatesTags: ['Pack'],
-        }),
-
-        allPacks: builder.query({
-            query: () => ({
-                url: '/pack',
-                method: 'GET',
-            }),
-        }),
-
-
-        getProducerPack: builder.query({
-            query: (id: string) => ({
-                url: `/pack/${id}`,
-                method: 'GET',
-            }),
-        }),
-
-
-
-        getPackDetails: builder.query({
-            query: (id: string) => ({
-                url: `/pack/single-pack/${id}`,
-                method: 'GET',
-            }),
-        }),
-
-
-        deletePack: builder.mutation({
-            query: (id: string) => ({
-                url: `/pack/${id}`,
-                method: 'DELETE',
-            }),
-        }),
-
-
-
+  endpoints: (builder) => ({
+    createPack: builder.mutation({
+      query: (formData: FormData) => ({
+        url: "/pack/create-pack",
+        method: "POST",
+        body: formData,
+      }),
+      invalidatesTags: ["Pack"],
     }),
-    overrideExisting: true,
+
+    allPacks: builder.query({
+      query: () => ({
+        url: "/pack",
+        method: "GET",
+      }),
+    }),
+
+    getProducerPack: builder.query({
+      query: (id: string) => ({
+        url: `/pack/${id}`,
+        method: "GET",
+      }),
+    }),
+
+    getPackDetails: builder.query({
+      query: (id: string) => ({
+        url: `/pack/single-pack/${id}`,
+        method: "GET",
+      }),
+    }),
+
+    updatePack: builder.mutation({
+      query: ({ id, formData }: { id: string; formData: FormData }) => ({
+        url: `/pack/update-pack/${id}`,
+        method: "PATCH",
+        body: formData,
+      }),
+    }),
+
+    deletePack: builder.mutation({
+      query: (id: string) => ({
+        url: `/pack/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+  overrideExisting: true,
 });
 
-export const { useCreatePackMutation, useGetProducerPackQuery, useGetPackDetailsQuery, useDeletePackMutation, useAllPacksQuery } = packApis;
+export const {
+  useCreatePackMutation,
+  useGetProducerPackQuery,
+  useGetPackDetailsQuery,
+  useDeletePackMutation,
+  useAllPacksQuery,
+  useUpdatePackMutation,
+} = packApis;
